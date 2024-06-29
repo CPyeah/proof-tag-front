4589a2be09ccb74b239d28a1a0477df93258fdfd
chengpeng <8103313+chengpengyeah@user.noreply.gitee.com>
2024-06-29 23:43:41 +0800

    dd

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
 import ValidateView from '../views/ValidateView.vue'
 
 
@@ -7,7 +7,7 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: ValidateView
   },
   {
     path: '/validate',
